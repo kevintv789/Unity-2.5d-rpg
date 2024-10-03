6cacb6f67ff27d2f18d63e94b9c84bc37ff85fb7
kevintv789 <kevintv789@gmail.com>
2024-10-03 14:05:43 -0700

    added spawn points

--- 2.5DUnityCourse/Assets/Scripts/BattleSystem.cs
+++ 2.5DUnityCourse/Assets/Scripts/BattleSystem.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 public class BattleSystem : MonoBehaviour
 {
+    [Header("Battlers")]
     [SerializeField]
     private List<BattleEntities> allBattlers = new List<BattleEntities>();
 
@@ -11,6 +12,13 @@ public class BattleSystem : MonoBehaviour
 
     [SerializeField]
     private List<BattleEntities> enemyBattlers = new List<BattleEntities>();
+
+    [Header("Spawn Points")]
+    [SerializeField]
+    private Transform[] partySpawnPoints;
+
+    [SerializeField]
+    private Transform[] enemySpawnPoints;
 
     private PartyManager partyManager;
     private EnemyManager enemyManager;
@@ -30,6 +38,7 @@ public class BattleSystem : MonoBehaviour
         List<PartyMember> currentParty = partyManager.GetCurrentParty();
 
         // Create BattleEntities for each party member
+        int i = 0;
         foreach (PartyMember member in currentParty)
         {
             BattleEntities battler = new BattleEntities(
@@ -41,8 +50,20 @@ public class BattleSystem : MonoBehaviour
                 member.Strength,
                 true
             );
+
+            // Spawn battler at party spawn point
+            BattleVisuals tempBattleVisuals = Instantiate(
+                    member.MemberBattleVisualPrefab,
+                    partySpawnPoints[i].position,
+                    Quaternion.identity
+                )
+                .GetComponent<BattleVisuals>();
+            tempBattleVisuals.SetStartingValues(member.MaxHealth, member.MaxHealth, member.Level);
+            battler.SetBattleVisuals(tempBattleVisuals);
+
             playerBattlers.Add(battler);
             allBattlers.Add(battler);
+            i++;
         }
     }
 
@@ -52,6 +73,7 @@ public class BattleSystem : MonoBehaviour
         List<Enemy> currentEnemy = enemyManager.GetCurrentEnemies();
 
         // Create BattleEntities for each enemy
+        int i = 0;
         foreach (Enemy enemy in currentEnemy)
         {
             BattleEntities battler = new BattleEntities(
@@ -63,8 +85,21 @@ public class BattleSystem : MonoBehaviour
                 enemy.Strength,
                 false
             );
+
+            // Spawn battler at enemy spawn point
+            BattleVisuals tempBattleVisuals = Instantiate(
+                    enemy.EnemyBattleVisualPrefab,
+                    enemySpawnPoints[i].position,
+                    Quaternion.identity
+                )
+                .GetComponent<BattleVisuals>();
+
+            tempBattleVisuals.SetStartingValues(enemy.MaxHealth, enemy.MaxHealth, enemy.Level);
+            battler.SetBattleVisuals(tempBattleVisuals);
+
             enemyBattlers.Add(battler);
             allBattlers.Add(battler);
+            i++;
         }
     }
 }
@@ -79,6 +114,7 @@ public class BattleEntities
     public int Initiative;
     public int Strength;
     public bool IsPlayer;
+    public BattleVisuals BattleVisuals;
 
     public BattleEntities(
         string name,
@@ -97,5 +133,10 @@ public class BattleEntities
         Initiative = initiative;
         Strength = strength;
         IsPlayer = isPlayer;
+    }
+
+    public void SetBattleVisuals(BattleVisuals battleVisuals)
+    {
+        BattleVisuals = battleVisuals;
     }
 }
